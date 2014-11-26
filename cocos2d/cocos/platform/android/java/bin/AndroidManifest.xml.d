b0c5522e4ef397b7e5a1a18c7cf2a0185091b361
乔书军 <shujun.qiao@chukong-inc.com>
2014-11-26 18:18:02 +0800

    add files to project.

---
+++ cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/workspace/anysdk/cpp/hello/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/workspace/anysdk/cpp/hello/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
---
+++ cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /workspace/anysdk/cpp/hello/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
